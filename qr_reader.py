54ede7d96b0a7008f6a6b3c44377fe75385c5257
burak-kara <burak.kara@ozu.edu.tr>
2021-01-16 01:17:48 +0300

    implemented qr generator with js. created a html page to give it to the OBS. modified QR reader

--- qr_reader.py
+++ qr_reader.py
@@ -1,7 +1,9 @@
 import cv2 as cv
 import numpy as np
 from PIL import ImageGrab
+import matplotlib.pyplot as plt
 import datetime
+import time
 
 
 # Display barcode and QR code location
@@ -15,23 +17,21 @@ def display(im, bbox):
 
 
 while True:
-    screen = ImageGrab.grab()
-    img = np.array(screen.getdata(), dtype='uint8')
-    img = img.reshape((screen.size[1], screen.size[0], 3))
-    w, h = screen.size
-    w = int(w / 2)
-    h = int(h / 2)
-    img = cv.resize(img, (w, h))
-    # cv.imshow('window', cv.cvtColor(img, cv.CV_32F))
+    img = ImageGrab.grab()
+    img_np = np.array(img)
+    img = cv.cvtColor(img_np, cv.COLOR_BGR2GRAY)
+    # cv.imshow('window', img)
 
     qrDecoder = cv.QRCodeDetector()
 
     # Detect and decode the qrcode
     data, bbox, rectifiedImage = qrDecoder.detectAndDecode(img)
     if len(data) > 0:
-        ts = datetime.datetime.now()
-        print("Decoded Time : {}".format(data))
-        print("Current Time : {}".format(ts))
+        ts = time.time()
+        # print("Decoded Time : {}".format(data))
+        # print("Current Time : {}".format(ts))
+        diff = abs(round(float(ts) - float(data) - 0.2, 1))
+        print("Diff : {}".format(diff))
         # display(img, bbox)
         # rectifiedImage = np.uint8(rectifiedImage)
         # cv.imshow("Rectified QRCode", rectifiedImage)
